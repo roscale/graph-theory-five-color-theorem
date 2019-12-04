710f43dc605a173bd161ac60b76b076d65294688
Rosca Alex <roscaalex19@gmail.com>
2019-12-04 21:08:00 +0100

    Add teacher's graph implementation

---
+++ graph.h
@@ -0,0 +1,48 @@
+#if defined(GRAPHES_H)
+#else
+#define GRAPHES_H
+
+#define MAX 10000
+
+struct eltadj {
+   int dest;
+   int info;
+   struct eltadj *suivant;
+};
+
+struct sommet {
+   int label;
+   int info;
+   struct sommet *suivant;
+   struct eltadj *adj;
+};
+
+struct graphe {
+   int nbS;
+   int nbA;
+   int maxS;
+   struct sommet *premierSommet;
+   struct sommet *dernierSommet;
+};
+
+typedef struct graphe GRAPHE;
+typedef struct sommet SOMMET;
+typedef struct eltadj ELTADJ;
+
+void initialiserGraphe(GRAPHE *);
+
+int ajouterSommet(GRAPHE *, int info);
+
+int ajouterArc(GRAPHE *, int a, int b, int info);
+
+int supprimerSommet(GRAPHE *, int label);
+
+int supprimerArc(GRAPHE *, int a, int b);
+
+void supprimerGraphe(GRAPHE *);
+
+void afficherGraphe(GRAPHE *);
+
+int lireFichier(char *nomf, GRAPHE *);
+
+#endif
