9e1abeb6736292abc7f51f22595e624ac3b43c0f
Rosca Alex <roscaalex19@gmail.com>
2019-12-09 17:15:02 +0100

    Extract code into functions

--- main.c
+++ main.c
@@ -1,14 +1,91 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
 #include "Stack.h"
 #include "graph.h"
 
-//https://en.wikipedia.org/wiki/Five_color_theorem?fbclid=IwAR2gWXQwMi2bhCh9HIW6gnY_vz_qEf5Cwk5j-c-YYZk23LTf-KPZSMsBNJY
+// https://en.wikipedia.org/wiki/Five_color_theorem?fbclid=IwAR2gWXQwMi2bhCh9HIW6gnY_vz_qEf5Cwk5j-c-YYZk23LTf-KPZSMsBNJY
+
+enum Color {
+   RED,
+   GREEN,
+   BLUE,
+   YELLOW,
+   PURPLE
+};
+
+const char *colorToString(int color) {
+	switch (color) {
+		case RED: return "RED";
+		case GREEN: return "GREEN";
+		case BLUE: return "BLUE";
+		case YELLOW: return "YELLOW";
+		case PURPLE: return "PURPLE";
+		default: assert(false && "Invalid color");
+	}
+}
+
+// TODO Make the adjacency list have pointers to vertices, not labels.
+// Otherwise, we have a O(n^2) complexity
+SOMMET *findVertexWithLabel(GRAPHE *graph, int label) {
+	SOMMET *foundVertex = NULL;
+	for (SOMMET *v = graph->premierSommet;
+	     v != NULL; v = v->suivant) {
+		// Search the vertex with the same label
+		// Each vertex has a unique label
+		if (v->label == label) {
+			foundVertex = v;
+			break;
+		}
+	}
+	assert(foundVertex != NULL);
+	return foundVertex;
+}
+
+bool vertexMatchesConditionsForS4(SOMMET *vertex) {
+	// Degree at most 4
+	return vertex->degree <= 4;
+}
+
+bool vertexMatchesConditionsForS5(GRAPHE *graph, SOMMET *vertex) {
+	// Degree five
+	if (vertex->degree == 5) {
+		// and at least one adjacent vertex with degree at most six
+		for (ELTADJ *adj = vertex->adj; adj != NULL; adj = adj->suivant) {
+			SOMMET *neighbour = findVertexWithLabel(graph, adj->dest);
+			printf("Neighbour of %i: %i\n", vertex->label, neighbour->label);
+
+			if (neighbour->degree <= 6) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+void populateStacks(GRAPHE *graph, Stack *s4, Stack *s5) {
+	for (SOMMET *v = graph->premierSommet; v != NULL; v = v->suivant) {
+		if (vertexMatchesConditionsForS4(v)) {
+			pushStack(s4, v);
+		}
+		else if (vertexMatchesConditionsForS5(graph, v)) {
+			pushStack(s5, v);
+		}
+	}
+}
+
+void mergeVertices(GRAPHE *graph, Stack *s5, Stack *sd) {
+	assert(!isStackEmpty(s5));
+	SOMMET *v = popStack(s5);
+}
+
 int main() {
 	Stack *s4 = createStack(100);
 	Stack *s5 = createStack(100);
 	Stack *sd = createStack(100);
 
+	// Graph example
 	GRAPHE *graph = malloc(sizeof(GRAPHE));
 	initialiserGraphe(graph);
 
@@ -35,23 +112,22 @@ int main() {
 	printf("Initial graph:\n");
 	afficherGraphe(graph);
 
-	// TODO remove loops
-	printf("Vertex list:\n");
-	for (SOMMET *v = graph->premierSommet; v != NULL; v = v->suivant) {
-		// Degree at most 4
-		if (v->degree <= 4) {
-			pushStack(s4, v);
-			printf("We push. Degree : %d \n", v->degree);
-		}
-			// Degree five
-		else if (v->degree == 5) {
-			// and at least one adjacent vertex with degree at most six
-			for (ELTADJ *adj = v->adj; adj != NULL; adj = adj->suivant) {
-				printf("Neighbour of %i: %i\n", v->label, adj->dest);
-			}
-		}
+	// STEP 1
+	populateStacks(graph, s4, s5);
+
+	// STEP 2
+	// TODO
+
+	// STEP 3
+	assert(isStackEmpty(s4)); // deg(graph) >= 5
+
+	// The graph is empty
+	if (graph->nbS == 0) {
+		// STEP 5
+		// TODO
+	} else {
+		mergeVertices(graph, s5, sd);
 	}
-	// Test push raul
 
 	return 0;
 }
